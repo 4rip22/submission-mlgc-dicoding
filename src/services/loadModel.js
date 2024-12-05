c2975aa35a3ba58fe173b87ee961271977595c3f
4rip22 <kkruipputra816@gmail.com>
2024-12-06 02:18:00 +0700

    fix:code-finally-update

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,5 @@
 const tf = require('@tensorflow/tfjs-node');
 async function loadModel() {
-    return tf.loadGraphModel('https://storage.googleapis.com/model-mlgc-bucket/model.json');
+    return tf.loadGraphModel('https://');// isi sesuai bucket model jika tidak menggunakan env
 }
 module.exports = loadModel;
